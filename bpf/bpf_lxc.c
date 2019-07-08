geekhuyang/cilium

commit 867fc862c71cb174d0f196214b179bafc4d0b6b4

datapath: Avoid MASQing NodePort replies
This commit introduces a new skb mark which denotes that a packet
is a reply to a NodePort request.

This is needed, as all NodePort requests (but not replies)
bypass netfilter conntrack table and due to some weird nf_conntrack
logic starting with second reply, replies get MASQUERADed.

The mark makes such replies to be excluded from the MASQ rule.

Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/bpf_lxc.c
@@ -205,8 +205,10 @@ ct_recreate6:
 
 #ifdef ENABLE_NODEPORT
 		/* See comment in handle_ipv4_from_lxc(). */
-		if (ct_state.node_port)
+		if (ct_state.node_port) {
+			skb->mark |= MARK_MAGIC_NODE_PORT_REPLY;
 			return redirect(NATIVE_DEV_IFINDEX, 0);
+		}
 #endif
 		if (ct_state.rev_nat_index) {
 			ret = lb6_rev_nat(skb, l4_off, &csum_off,

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=e5ca11923c89f8a2d16daa25cb738f37d932d206
+GO_BINDATA_SHA1SUM=f73730e606d6e2db7ed38c9ed1c3af2b465c621d
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

pkg/datapath/iptables/iptables.go
@@ -554,6 +554,7 @@ func (m *IptablesManager) InstallRules(ifName string) error {
 			// * May not leave on a cilium_ interface, this excludes all
 			//   tunnel traffic
 			// * Must originate from an IP in the local allocation range
+			// * Must not be reply to BPF NodePort request
 			// * Tunnel mode:
 			//   * May not be targeted to an IP in the local allocation
 			//     range
@@ -570,14 +571,23 @@ func (m *IptablesManager) InstallRules(ifName string) error {
 					return err
 				}
 			} else {
-				if err := runProg("iptables", []string{
+				rule := []string{
 					"-t", "nat",
 					"-A", ciliumPostNatChain,
 					"-s", node.GetIPv4AllocRange().String(),
 					"!", "-d", remoteSnatDstAddrExclusion(),
 					"!", "-o", "cilium_+",
+				}
+				if option.Config.EnableNodePort {
+					nodePortReply := fmt.Sprintf("%#08x", linux_defaults.MagicMarkNodePortReply)
+					rule = append(rule,
+						"-m", "mark", "--mark", nodePortReply)
+				}
+				rule = append(rule,
 					"-m", "comment", "--comment", "cilium masquerade non-cluster",
-					"-j", "MASQUERADE"}, false); err != nil {
+					"-j", "MASQUERADE")
+
+				if err := runProg("iptables", rule, false); err != nil {
 					return err
 				}
 			}

bpf/lib/common.h
@@ -338,6 +338,7 @@ enum {
 #define MARK_MAGIC_KEY_ID		0xF000
 #define MARK_MAGIC_KEY_MASK		0xFF00
 
+#define MARK_MAGIC_NODE_PORT_REPLY	0x0500
 /**
  * get_identity - returns source identity from the mark field
  */

pkg/datapath/linux/linux_defaults/mark.go
@@ -42,7 +42,7 @@ package linux_defaults
 // 1 1 0 0 From host
 // 0 0 1 0 To Ingress Proxy
 // 0 0 1 1 To Egress proxy
-//
+// 0 1 0 1 NodePort reply
 const (
 	// MagicMarkHostMask can be used to fetch the host/proxy-relevant magic
 	// bits from a mark.
@@ -61,6 +61,10 @@ const (
 	// to determine whether the mark is indicating that traffic is destined
 	// to a proxy.
 	MagicMarkIsToProxy uint32 = 0x0200
+
+	// MagicMarkNodePortReply determines that the packet is a reply to
+	// a NodePort request.
+	MagicMarkNodePortReply int = 0x0500
 
 	// MagicMarkIngress determines that the traffic is sourced from the
 	// proxy which is applying Ingress policy

commit 9872527ca72c51b8c7c13fb8ac3e4811c91624c6

datapath: Fix NodePort reply mark rule
Previously, the mark mask was missing, so all bytes of the skb mark
were matched against the given mark, which made the rule to be bypassed
by all traffic.

Also, in the case of IPv4, the mask was not set.

Fixes: 867fc862 ("datapath: Avoid MASQing NodePort replies")
Signed-off-by: Martynas Pumputis <m@lambda.lt>

bpf/bpf_lxc.c
@@ -524,6 +524,7 @@ ct_recreate4:
 		 * perform the reverse DNAT.
 		 */
 		if (ct_state.node_port) {
+			skb->mark |= MARK_MAGIC_NODE_PORT_REPLY;
 			return redirect(NATIVE_DEV_IFINDEX, 0);
 		}
 #endif /* ENABLE_NODEPORT */

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=f73730e606d6e2db7ed38c9ed1c3af2b465c621d
+GO_BINDATA_SHA1SUM=f0df51b22cc7dbee916357893c4aeb7cb2982024
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

pkg/datapath/iptables/iptables.go
@@ -579,9 +579,11 @@ func (m *IptablesManager) InstallRules(ifName string) error {
 					"!", "-o", "cilium_+",
 				}
 				if option.Config.EnableNodePort {
-					nodePortReply := fmt.Sprintf("%#08x", linux_defaults.MagicMarkNodePortReply)
+					nodePortReply := fmt.Sprintf("%#08x/%#08x",
+						linux_defaults.MagicMarkNodePortReply,
+						linux_defaults.MagicMarkNodePortReply)
 					rule = append(rule,
-						"-m", "mark", "--mark", nodePortReply)
+						"-m", "mark", "!", "--mark", nodePortReply)
 				}
 				rule = append(rule,
 					"-m", "comment", "--comment", "cilium masquerade non-cluster",